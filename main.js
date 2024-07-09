26a47201a4f12f2bc587ee8bf271d4c55b61acc0
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-07-09 20:27:05 +0500

    for loop
    A for loop iterates through a sequence of elements, executing a block of code for each element until the sequence is exhausted or a condition is met.

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-import inquirer from "inquirer";
-//user variable
-let todo = [];
-while (true) {
-    let input = await inquirer.prompt([
-        {
-            name: "todoitem",
-            type: "input",
-            message: 'add todo item in listing'
-        },
-        {
-            name: "addmore",
-            type: 'list',
-            choices: ["yes", "No"]
-        }
-    ]);
-    // output  of program.
-    const { todoitem, addmore } = input;
-    todo.push(todoitem);
-    if (addmore == "no") {
-        console.log("todo listing");
-        //add more item
-        for (let i = 0; i < todo.length; i++) {
-            console.log(todo[i]);
-        }
-        // exit loop
-        break;
-    }
+"use strict";
+// Example of a for loop
+for (let i = 0; i < 5; i++) {
+    console.log(i); // Output: 0, 1, 2, 3, 4
+}
+// Example of a reverse for loop
+for (let i = 4; i >= 0; i--) {
+    console.log(i); // Output: 4, 3, 2, 1, 0
+}
+let arrays = [10, 20, 30, 40, 50];
+// Iterate over the array elements
+for (let i = 0; i < arrays.length; i++) {
+    console.log(arrays[i]); // Output: 10, 20, 30, 40, 50
+}
+// Reverse loop over the array elements
+for (let i = arrays.length - 1; i >= 0; i--) {
+    console.log(arrays[i]); // Output: 50, 40, 30, 20, 10
+}
+let array = [10, 20, 30, 40, 50];
+// Using for...of to iterate forward
+for (let num of array) {
+    console.log(num); // Output: 10, 20, 30, 40, 50
+}
+// Reverse iteration using for...of (not directly supported, but can be achieved with reverse array)
+for (let num of array.reverse()) {
+    console.log(num); // Output: 50, 40, 30, 20, 10
 }
