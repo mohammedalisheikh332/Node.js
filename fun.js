03d1ce2691c3598febb36099cabe219d73e07b9f
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-07-09 20:15:00 +0500

    Function
    A one-line function is a concise piece of code that performs a specific task and typically consists of a single statement, often used for simple operations or lambda functions in programming languages.

---
+++ fun.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet() {
+    console.log("Assalam O Alakium");
+    console.log("how are you");
+    console.log("Which class are you in?");
+    greet();
+    function Ali() { }
+    sheikh(params, String);
+    {
+    }
+}
+function Add() { }
+number(num1, number, num2, number);
+{
+    const sum = num1 * num2;
+    console.log(`sum is equal to ${sum}`);
+}
+Add;
+Number(10, 3);
+function generate() { }
+table(number, number);
+{
+}
