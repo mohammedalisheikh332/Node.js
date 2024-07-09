b62338162ffb3b750bf1f1b1a7c4dccd20cdacbd
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-07-09 20:16:46 +0500

    BMi
    BMI (Body Mass Index) is a numerical value calculated from a person's weight and height, used to assess body composition and health risk related to weight status.

---
+++ bmi.js
@@ -0,0 +1,6 @@
+// BMI calculator
+let weight = 70;
+let height = 1.80;
+let bmi = weight / height * height;
+console.log(`Mohammed Ali sheikh your BMI is ${bmi}`);
+export {};
---
+++ bmi.ts
@@ -0,0 +1,8 @@
+// BMI calculator
+
+let weight = 70;
+let height = 1.80;
+
+let bmi = weight/ height*height
+
+console.log(`Mohammed Ali sheikh your BMI is ${bmi}`)
