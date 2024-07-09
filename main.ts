5077a6882cfe52c6e1681e67e5adaf8c73403589
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-07-09 20:28:54 +0500

    gobal var
    A global variable is a variable declared outside of any function in a program, accessible and modifiable throughout the entire codebase.

--- main.ts
+++ main.ts
@@ -1,35 +1,18 @@
-// Example of a for loop
-for (let i = 0; i < 5; i++) {
-    console.log(i); // Output: 0, 1, 2, 3, 4
+var gobalvar:string ="Welcome To Pakistan"
+function scope(){
+    console.log(gobalvar)
 }
+scope();
+   
+//// Local VS Gobal scope.
 
-// Example of a reverse for loop
-for (let i = 4; i >= 0; i--) {
-    console.log(i); // Output: 4, 3, 2, 1, 0
+// gobal scope
+
+// gobal variable can accesable ever
+
+var gobalvar = "25";
+function gobal(){
+
 }
-
-
-let arrays = [10, 20, 30, 40, 50];
-
-// Iterate over the array elements
-for (let i = 0; i < arrays.length; i++) {
-    console.log(arrays[i]); // Output: 10, 20, 30, 40, 50
-}
-
-// Reverse loop over the array elements
-for (let i = arrays.length - 1; i >= 0; i--) {
-    console.log(arrays[i]); // Output: 50, 40, 30, 20, 10
-}
-
-let array = [10, 20, 30, 40, 50];
-
-// Using for...of to iterate forward
-for (let num of array) {
-    console.log(num); // Output: 10, 20, 30, 40, 50
-}
-
-// Reverse iteration using for...of (not directly supported, but can be achieved with reverse array)
-for (let num of array.reverse()) {
-    console.log(num); // Output: 50, 40, 30, 20, 10
-}
-
+console.log(gobalvar)
+scope();
